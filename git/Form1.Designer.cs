aa8f9de552e85f1755b74dc611dd7822d5ce3291
yolito <cherry_iiiolo@hotmail.com>
2015-11-10 08:53:44 -0600

    >:D

--- git/Form1.Designer.cs
+++ git/Form1.Designer.cs
@@ -28,24 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(84, 57);
+            this.button1.BackColor = System.Drawing.Color.SeaGreen;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(126, 289);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(148, 98);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "OPRIMEME";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(515, 347);
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(982, 546);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
--- git/Form1.cs
+++ git/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace git
 {
@@ -15,12 +16,27 @@ namespace git
         public Form1()
         {
             InitializeComponent();
+            //Thread t = new Thread();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola github");
-            MessageBox.Show("Estoy en internet :D");
+    
+            System.Windows.Forms.Button[] Buttons = new System.Windows.Forms.Button[10];
+
+            for (int i = 0; i < 10; ++i)
+            {
+                Buttons[i] = new Button();
+                Buttons[i].Text = "?";
+                Buttons[i].BackColor = Color.Yellow;
+                Buttons[i].Location = new System.Drawing.Point(i*100 + 100,100);
+                this.Controls.Add(Buttons[i]); 
+                MessageBox.Show("Hola github");
+                MessageBox.Show("Estoy en internet :D");
+            }
+
+           
         }
     }
 }
